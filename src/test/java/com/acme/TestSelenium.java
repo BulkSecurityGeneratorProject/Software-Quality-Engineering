8f8b98482f19cf8d75a43f01b607922c089363a4
Tania Akter <tania9309@gmail.com>
2017-06-09 13:43:25 -0700

    Adding more test cases for going to acmepass, and changed the setup so that we don't have to wait for timeouts

--- src/test/java/com/acme/TestSelenium.java
+++ src/test/java/com/acme/TestSelenium.java
@@ -1,14 +1,17 @@
 package com.acme;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.io.File;
 import java.net.URL;
@@ -25,29 +28,48 @@ public class TestSelenium {
 
     @Before
     public void setUp() throws Exception {
-        //ChromeDriverService service = new ChromeDriverService.Builder()
-	//        .usingDriverExecutable(new File("chromedriver"))
-        //    .usingAnyFreePort()
-        //    .build();
+//        System.setProperty("webdriver.gecko.driver", "/home/tanjulia/Desktop/geckodriver"); // 64 Bit Linux
 
-        //service.start();
-
-        // driver = new HtmlUnitDriver(true);
-        driver = new RemoteWebDriver(new URL("http://localhost:22000"), DesiredCapabilities.chrome());
+//       driver = new RemoteWebDriver(new URL("http://localhost:22000"), DesiredCapabilities.chrome());
         url = "http://localhost:8080/#/";
+        driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
         }
 
     @Test
-    public void UserCanSignIn() throws Exception {
+    public void userCanSignIn() throws Exception {
         driver.get(url);
 
-        driver.findElement(By.linkText("Sign in")).click();
-	    driver.findElement(By.id("username")).sendKeys("tom.hayes@acme.com");
-    	driver.findElement(By.id("password")).sendKeys("superman");
-	    assertTrue(driver.findElement(By.id("account-menu")).isDisplayed());
+        // Sign in button
+        driver.findElement(By.id("login")).click();
+
+        // Enter credentials
+        driver.findElement(By.id("username")).sendKeys("frank.paul@acme.com");
+        driver.findElement(By.id("password")).sendKeys("starwars");
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+
+        WebElement submit = null;
+        for(WebElement button : buttons) {
+            System.out.println(button.getText());
+            if ("Sign in".equals(button.getText())) {
+                submit = button;
+            }
+        }
+
+        submit.click();
+
+        // Check that the AcmePass link exists on the banner.
+        WebElement acmepass = driver.findElement(By.xpath("//a[@ui-sref='acme-pass']"));
+        Assert.assertNotNull(acmepass);
+    }
+
+    @Test
+    public void userGoesToACMEpass() throws Exception{
+        userCanSignIn();
+        driver.findElement(By.xpath("//a[@ui-sref='acme-pass']")).click();
+        assertEquals(driver.getTitle(), "ACMEPasses");
     }
 
     @After
